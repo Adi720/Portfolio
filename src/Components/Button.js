fd68b70df42b5e5708c161811fc283b534915017
Aditya <adityamore360123@gmail.com>
2021-09-29 00:19:02 +0530

    project buttons categorized

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,19 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Button = ({ filter, button }) => {
     return (
-        <div>
+        <ButtonsStyled>
             {
                 button.map((but, i) => {
                     return (
-                        <button key={i} onClick={() => filter(but)}>
+                        <ButtonStyled key={i} onClick={() => filter(but)}>
                             {but}
-                        </button>
+                        </ButtonStyled>
                     )
                 })
             }
-        </div>
+        </ButtonsStyled>
     )
 }
+
+const ButtonStyled = styled.button`
+    outline: none;
+    border: none;
+    background-color: var(--background-light-color-2);
+    padding: .4rem 2rem;
+    font-size: inherit;
+    color: var(--white-color);
+    cursor: pointer;
+    transition: all .4s ease-in-out;
+    &:active ,&:focus{
+        background-color: var(--primary-color);
+    }
+    &:hover{
+        background-color: var(--primary-color);
+    }
+    &:not(:last-child){
+        margin-right: .6rem;
+    }
+`;
+
+const ButtonsStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 70%;
+    margin: 2.4rem auto;
+`;
 
 export default Button
